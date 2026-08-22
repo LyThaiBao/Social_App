104374c2d8abe8ca2785c53b241b64f9b6ef0163
timmy <baoly069@gmail.com>
2026-08-22 09:36:05 +0700

    refactor: post service (fix IDOR problem in modify post)

--- src/main/java/social_app/example/social_app/service/post/PostServiceImp.java
+++ src/main/java/social_app/example/social_app/service/post/PostServiceImp.java
@@ -11,6 +11,7 @@ import social_app.example.social_app.dto.friendship.FriendShipDetail;
 import social_app.example.social_app.dto.post.PostRequest;
 import social_app.example.social_app.dto.post.PostResponse;
 import social_app.example.social_app.entity.*;
+import social_app.example.social_app.exception.AuthException;
 import social_app.example.social_app.exception.ForbiddenException;
 import social_app.example.social_app.exception.NotFoundResource;
 import social_app.example.social_app.mapper.PostMapper;
@@ -81,8 +82,12 @@ public class PostServiceImp implements PostService{
 
     @Override
     @Transactional
-    public PostResponse modifyPost(Integer id,PostRequest request) {
+    public PostResponse modifyPost(Integer id,PostRequest request,Principal principal) {
         Posts post = this.postRepository.findById(id).orElseThrow(() -> new NotFoundResource("Not found post with id "+id));
+        String username = principal.getName();
+        if(!username.equals(post.getMember().getUser().getUsername())){
+            throw new AuthException("You do not have permission");
+        }
         if(request.getContent()!=null){
             post.setContent(request.getContent());
         }
